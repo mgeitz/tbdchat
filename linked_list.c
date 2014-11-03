73e04eea7971e3abd20e452b79b4a2cab555dc8b
Matt Owens <matthew.owens2017@gmail.com>
2014-11-03 14:28:40 -0500

    Created a c file and header file for the linked list we will use to store user registration information.

---
+++ linked_list.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "linked_list.h"
+
+void insert(User **head, User *new_user)
+{
+   User *temp = *head;
+   if(*head == NULL)
+   {
+      *head = new_user;
+      return;
+   } 
+
+   while(temp->next != NULL) temp = temp->next;
+
+   temp->next = new_user;
+   new_user->next = NULL;
+}
+
+char *get_real_name(User **head, char *user)
+{
+   char *error = "ERROR";
+   User *temp = *head;
+
+   if(*head == NULL) return error;
+
+   while(strcmp(user, temp->username) != 0)
+   {
+      if(temp->next == NULL) return error;
+      temp=temp->next;
+   }
+
+   return temp->real_name;
+}
+
---
+++ linked_list.h
@@ -0,0 +1,10 @@
+struct user {
+   char username[50];
+   char real_name[64];
+   struct user *next;
+};
+
+typedef struct user User;
+void insert(User **head, User *new_user);
+char *get_real_name(User **head, char *user);
+
