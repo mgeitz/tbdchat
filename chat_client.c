f9b5cb29226896490b94b540f76674be80db4fb4
Matt Owens <matthew.owens2017@gmail.com>
2014-11-04 16:15:50 -0500

    Added loop to ensure user enters a valid username on login.

--- chat_client.c
+++ chat_client.c
@@ -111,7 +111,6 @@ int main(int argc, char **argv)
          tx_pkt.timestamp = time(NULL);
          tx_pkt.options = 1;
          send(conn, (void *)&tx_pkt, sizeof(packet), 0);
-         loop_control = 0;
       }
 
       else if(selection == 0)
@@ -137,12 +136,19 @@ int main(int argc, char **argv)
          send(conn, (void *)&tx_pkt, sizeof(packet), 0);
 
 
-         loop_control = 0;
       }
 
       else
       {
          printf("Invalid input. Please try again\n");
+      }
+     
+      recv(conn, (void *)&tx_pkt, sizeof(packet),0);
+      if(strcmp(tx_pkt.buf, "ERROR") == 0) printf("Invalid username. Please try again.\n");
+      else 
+      {
+          printf("Your name is: %s\n", tx_pkt.buf);
+          loop_control = 0; 
       }
    }
      
